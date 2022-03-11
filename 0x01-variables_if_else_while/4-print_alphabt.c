42cffc68167acde10ad1261f94411a895e173f46
aliciahvivian <vivianalice11@gmail.com>
2022-03-12 00:22:23 +0300

    print in all lowercase letter but e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
- * Return: 0
- */
-int main(void)
+*main - main block
+*Description- prints the alphabet in lowercase,Print all the letters except q and e
+*Return: 0
+*/
+int main (void)
 {
-	char letter = 'a'; 
-	while(letter <= 'z')
+	char letter = 'a';
+	while (letter <= 'z')
 	{
-		if (letter != 'e' && letter != 'q')
+		if (letter != 'e' && letter = 'q')
 		{
 			putchar(letter);
 		}
